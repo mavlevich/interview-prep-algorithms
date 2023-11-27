4f018b245cc5ff537e5fe9c4c3cb66eef0c20d22
mavlevich <mavlevichx@email.com>
2023-11-27 16:41:50 +0100

    fix Task1

--- src/Task1.java
+++ src/Task1.java
@@ -1,2 +1,40 @@
 public class Task1 {
+    // Метод для слияния двух отсортированных массивов
+    public static int[] mergeSortedArrays(int[] array1, int[] array2) {
+        int m = array1.length;
+        int n = array2.length;
+
+        // Создаем новый массив для объединения
+        int[] mergedArray = new int[m + n];
+
+        int i = 0, j = 0, k = 0;
+
+        // Слияние массивов
+        while (i < m && j < n) {
+            if (array1[i] <= array2[j]) {
+                mergedArray[k] = array1[i];
+                i++;
+            } else {
+                mergedArray[k] = array2[j];
+                j++;
+            }
+            k++;
+        }
+
+        // Добавление оставшихся элементов из array1 (если есть)
+        while (i < m) {
+            mergedArray[k] = array1[i];
+            i++;
+            k++;
+        }
+
+        // Добавление оставшихся элементов из array2 (если есть)
+        while (j < n) {
+            mergedArray[k] = array2[j];
+            j++;
+            k++;
+        }
+
+        return mergedArray;
+    }
 }
