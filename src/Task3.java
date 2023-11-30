b3cbdf9bdc15509148cea9f63c453ced5838c77f
mavlevich <mavlevichx@gmail.com>
2023-11-30 02:39:55 +0100

    add a solution to Task3

--- src/Task3.java
+++ src/Task3.java
@@ -1,2 +1,15 @@
+// Reverse words in a given string
+// Example "I love programming" -> "programming love I"
+
 public class Task3 {
+    public static String reverseWordsInSentence(String sentence) {
+        String[] words = sentence.split(" ");
+        String reversed = "";
+
+        for (int i = words.length - 1; i >= 0; i--) {
+            reversed += words[i] + " ";
+        }
+
+        return reversed;
+    }
 }
